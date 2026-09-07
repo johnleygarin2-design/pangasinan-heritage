f84b67900f03f18374343e60e510597a8d28fd98
JL <johnleygarin2@gmail.com>
2026-09-08 00:33:56 +0800

    Fix GitHub Pages configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: "/pangasinan-heritage",
-  assetPrefix: "/pangasinan-heritage/",
+  output: 'export',
+  basePath: '/pangasinan-heritage',
+  assetPrefix: '/pangasinan-heritage/',
   images: {
     unoptimized: true,
   },
 };
-export default nextConfig;
+
+module.exports = nextConfig;
