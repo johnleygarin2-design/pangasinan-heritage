064d4f3375c7283de928fe7e345a6655374e6ce2
johnleygarin2-design <johnleygarin2@gmail.com>
2026-09-10 11:52:29 +0800

    Fix image paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-
-  basePath:
-    process.env.NODE_ENV === "production"
-      ? "/pangasinan-heritage"
-      : "",
-
+  basePath: "/pangasinan-heritage",
+  assetPrefix: "/pangasinan-heritage/",
   images: {
     unoptimized: true,
   },
-
-  trailingSlash: true,
 };
 
 export default nextConfig;
